d265b8f0b425bff464e8dc29f46944789339bb1c
Mantton <mantton.official@gmail.com>
2023-06-01 18:12:19 -0400

    ref: delete history files

---
+++ Shared/Views/History/Categories/History+ContentSource.swift
@@ -0,0 +1,56 @@
+//
+//  History+ContentSource.swift
+//  Suwatte (iOS)
+//
+//  Created by Mantton on 2023-05-30.
+//
+
+import SwiftUI
+
+
+
+extension HistoryView {
+    
+    struct ContentSourceCell: View {
+        var marker: ProgressMarker
+        var content: StoredContent
+        var chapter: ChapterReference
+        var size = 140.0
+        
+        var body: some View {
+            HStack {
+                STTImageView(url: URL(string: content.cover), identifier: content.ContentIdentifier)
+                    .frame(minWidth: 0, idealWidth: size, maxWidth: size, minHeight: 0, idealHeight: size * 1.5, maxHeight: size * 1.5, alignment: .center)
+                    .scaledToFit()
+                    .cornerRadius(5)
+                    .shadow(radius: 3)
+                
+                VStack(alignment: .leading, spacing: 7) {
+                    Text(content.title)
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .lineLimit(3)
+                    VStack(alignment: .leading) {
+                        Text(chapter.chapterName)
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(.gray)
+                        
+                        if let dateRead = marker.dateRead {
+                            Text(dateRead.timeAgo())
+                                .font(.footnote)
+                                .fontWeight(.light)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Spacer()
+                    
+                }
+                .frame(minHeight: 0, idealHeight: size * 1.5, maxHeight: size * 1.5, alignment: .center)
+                .padding(.top, 1.5)
+                Spacer()
+                HistoryView.ProgressIndicator(progress: marker.isCompleted ? 1.0 : marker.progress ?? 0.0)
+            }
+        }
+    }
+}
