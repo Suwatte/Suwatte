ccd3855c1660d9c14b1351f6d7a26080460dc353
mantton <mantton.official@gmail.com>
2023-09-15 11:58:21 -0400

    fix: misc bugs

--- iOS/Views/Browse/BrowseView.swift
+++ iOS/Views/Browse/BrowseView.swift
@@ -94,6 +94,7 @@ struct BrowseView: View {
             guard !hasLoaded else { return }
             await model.observe()
             checkLists()
+            hasLoaded = true
         }
         .onDisappear {
             Task {
