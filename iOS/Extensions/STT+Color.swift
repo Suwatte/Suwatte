63402bb7638958dd6e701440f0136b29c0adc3af
mantton <mantton.official@gmail.com>
2023-10-01 03:27:57 -0400

    fix: warnings

--- iOS/Extensions/STT+Color.swift
+++ iOS/Extensions/STT+Color.swift
@@ -112,7 +112,7 @@ extension Color: RawRepresentable, UserDefaultsSerializable {
         }
 
         do {
-            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .black
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data) ?? .black
             self = Color(color)
         } catch {
             self = .black
