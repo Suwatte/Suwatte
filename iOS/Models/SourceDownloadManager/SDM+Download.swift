5cd6cbd7c3870e637f2c9af02d765a29c3401097
Seyden <saiifii@live.de>
2024-09-05 00:37:29 +0200

    SDM: Set download task to failed if task wasn't successful instead of just returning (#124)

--- iOS/Models/SourceDownloadManager/SDM+Download.swift
+++ iOS/Models/SourceDownloadManager/SDM+Download.swift
@@ -49,8 +49,7 @@ extension SDM {
             announce(id, state: .downloading(progress: 0.0))
             let completion = try await handleFetchResponse(id: id, urls: data.urls, raws: data.raws, text: data.text)
 
-            guard completion else { return }
-            await didFinishTasksAtHead(id: id, with: .completed)
+            await didFinishTasksAtHead(id: id, with: completion ? .completed : .failed)
         } catch {
             Logger.shared.error(error, CONTEXT)
             await didFinishTasksAtHead(id: id, with: .failed)
