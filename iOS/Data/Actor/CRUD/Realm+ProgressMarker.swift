02ed8763f20dce967eda3e2bb55b923a65e09e1c
Seyden <saiifii@live.de>
2024-09-04 13:07:18 +0200

    TrackerSync: Fix start reading a chapter setting the progress always to the next chapter

--- iOS/Data/Actor/CRUD/Realm+ProgressMarker.swift
+++ iOS/Data/Actor/CRUD/Realm+ProgressMarker.swift
@@ -17,12 +17,17 @@ extension RealmActor {
         return progressMarkers
     }
 
-    func getFrozenContentMarkers(for contentIdentifier: ContentIdentifier) -> [ProgressMarker] {
-        let progressMarkers = getContentMarkers(for: contentIdentifier)
-            .freeze()
-            .toArray()
+    func getFrozenContentMarkers(for contentIdentifier: ContentIdentifier, completed: Bool? = nil) -> [ProgressMarker] {
+        var progressMarkers = getContentMarkers(for: contentIdentifier)
+        if completed != nil {
+            let pred = NSPredicate(format: "chapter != null && isDeleted == false && \(completed! ? "" : "not")(totalPageCount >= 1 && lastPageRead == totalPageCount)")
+            progressMarkers = progressMarkers
+                .filter(pred)
+        }
 
         return progressMarkers
+            .freeze()
+            .toArray()
     }
 
     func getFrozenContentMarker(for chapterId: String) -> ProgressMarker? {
@@ -55,13 +60,13 @@ extension RealmActor {
         return getLatestReadContentMarker(for: contentIdentifier)?.freeze()
     }
 
-    func getMaxReadContentMarker(for contentIdentifier: ContentIdentifier) -> ProgressMarker? {
-        return getFrozenContentMarkers(for: contentIdentifier)
+    func getMaxReadContentMarker(for contentIdentifier: ContentIdentifier, completed: Bool? = nil) -> ProgressMarker? {
+        return getFrozenContentMarkers(for: contentIdentifier, completed: completed)
             .max(by: \.chapter!.chapterOrderKey)
     }
 
-    func getMaxReadKey(for contentIdentifier: ContentIdentifier) -> Double {
-        getMaxReadContentMarker(for: contentIdentifier)?.chapter?.chapterOrderKey ?? 0
+    func getMaxReadKey(for contentIdentifier: ContentIdentifier, completed: Bool? = nil) -> Double {
+        getMaxReadContentMarker(for: contentIdentifier, completed: completed)?.chapter?.chapterOrderKey ?? 0
     }
 
     func didCompleteChapter(chapter: ThreadSafeChapter) async {
@@ -230,8 +235,8 @@ extension RealmActor {
 
 extension RealmActor {
     /// Fetches the highest marked chapter with respect to content links
-    func getMaxReadChapterOrderKey(for contentIdentifier: ContentIdentifier) -> Double {
-        let maxReadOnTarget = getMaxReadKey(for: contentIdentifier)
+    func getMaxReadChapterOrderKey(for contentIdentifier: ContentIdentifier, completed: Bool? = nil) -> Double {
+        let maxReadOnTarget = getMaxReadKey(for: contentIdentifier, completed: completed)
         let maxReadOnLinked =
             getLinkedContent(for: contentIdentifier.id)
             .map { getMaxReadKey(for: $0.ContentIdentifier) }
